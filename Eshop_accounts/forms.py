88dee679c2889ad580c59b03e6fa2ac000c0458f
amofogh <amir.ofogh81@gmail.com>
2021-11-30 16:59:55 +0330

    register error

--- Eshop_accounts/forms.py
+++ Eshop_accounts/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core import validators
 
 
 class Login_Form(forms.Form):
@@ -6,3 +8,54 @@ class Login_Form(forms.Form):
                                widget=forms.TextInput(attrs={'placeholder': 'نام کاربری خود را وارد کنید'}))
     password = forms.CharField(label='کلمه عبور',
                                widget=forms.PasswordInput(attrs={'placeholder': 'رمز خود را وارد کنید'}))
+    remember_me = forms.BooleanField(required=False)
+
+    def clean_username(self):
+        user = self.cleaned_data.get('username')
+        # user = self.cleaned_data.get('username')
+        existence = User.objects.filter(username=user).exists()
+        if not existence:
+            raise forms.ValidationError('نام کاربری یا کلمه عبور صحیح نمی باشید')
+        return user
+
+
+class Register_Form(forms.Form):
+    username = forms.CharField(label='نام کاربری',
+                               widget=forms.TextInput(attrs={'placeholder': 'نام کاربری'})
+                               )
+    email = forms.EmailField(label='ایمیل',
+                             widget=forms.EmailInput(attrs={'placeholder': 'ایمیل'})
+                             )
+    first_name = forms.CharField(label='نام',
+                                 widget=forms.TextInput(attrs={'placeholder': 'نام'})
+                                 )
+    last_name = forms.CharField(label='نام خوانوادگی',
+                                widget=forms.TextInput(attrs={'placeholder': 'نام خوانوادگی'})
+                                )
+    password = forms.CharField(label='کلمه عبور',
+                               widget=forms.PasswordInput(attrs={'placeholder': 'کلمه عبور'}),
+                               validators=[
+                                   validators.RegexValidator(r'^\d*$', 'کلمه عبور باید متشکل از عدد و حروف باشد'),
+                                   # validators.MinValueValidator(limit_value=8,
+                                   #                              message='حداقل تعداد کلمه عبور باید 8 رقم باشد',
+                                   #
+                                   #
+                                   #                              )
+
+                               ]
+                               )
+    re_password = forms.CharField(label='تکرار کلمه عبور',
+                                  widget=forms.PasswordInput(attrs={'placeholder': 'تکرار کلمه عبور'})
+                                  )
+
+    def clean_re_password(self):
+        print('clean started')
+        password = self.cleaned_data.get('password')
+        re_password = self.cleaned_data.get('re_password')
+        print(re_password)
+        print(password)
+        if password != re_password:
+            print('here')
+            raise forms.ValidationError('کلمه های عبور مغایرت دارد')
+
+        return password
--- Eshop_accounts/views.py
+++ Eshop_accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import Login_Form
+from django.contrib.auth.models import User
+from .forms import Login_Form, Register_Form
 
 
 # Create your views here.
@@ -13,10 +14,15 @@ def login_user(request):
     if loginform.is_valid():
         username = loginform.cleaned_data['username']
         password = loginform.cleaned_data['password']
+        remember = loginform.cleaned_data['remember_me']
         user = authenticate(request, password=password, username=username)
-        if user is not None:
+        if user:
             login(request, user)
+            if not remember:
+                request.session.set_expiry(0)  # after session closed user logout of the account
             return redirect('/')
+        else:
+            loginform.add_error('username', 'نام کاربری یا کلمه عبور نادرست است')
     context = {
         'login_form': loginform
     }
@@ -24,5 +30,25 @@ def login_user(request):
 
 
 def register_user(request):
-    context = {}
+    if request.user.is_authenticated:
+        return redirect('/')
+
+    register_form = Register_Form(request.POST or None)
+
+    if register_form.is_valid():
+        username = register_form.cleaned_data.get('username')
+        email = register_form.cleaned_data.get('email')
+        password = register_form.cleaned_data.get('password')
+        first_name = register_form.cleaned_data.get('first_name')
+        last_name = register_form.cleaned_data.get('last_name')
+        User.objects.create_user(username=username,
+                                 email=email,
+                                 password=password,
+                                 first_name=first_name,
+                                 last_name=last_name)
+        return redirect('/')
+
+    context = {
+        'register_form': register_form
+    }
     return render(request, 'accounts/register.html', context)
